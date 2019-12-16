427ee08a09d0d6f9654ad6fda2463d922a7b4651
Lurivar <lurivar@users.noreply.github.com>
2019-12-16 16:14:44 +0100

    Update sql

---
+++ Config/update/1.4.0.sql
@@ -0,0 +1,23 @@
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ---------------------------------------------------------------------
+-- mailjet_contact_list
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `mailjet_contact_list`;
+
+CREATE TABLE `mailjet_contact_list`
+(
+    `id_cl` INTEGER NOT NULL AUTO_INCREMENT,
+    `name_cl` VARCHAR(255) NOT NULL,
+    `slug_cl` VARCHAR(255) NOT NULL,
+    `default_list` TINYINT(1) NOT NULL,
+    PRIMARY KEY (`id_cl`),
+    UNIQUE INDEX `name_UNIQUE` (`name_cl`),
+    UNIQUE INDEX `slug_UNIQUE` (`slug_cl`)
+) ENGINE=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
