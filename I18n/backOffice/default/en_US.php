1c67270c05d75b2f49d746e3a521362f9ada79a1
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-29 09:40:20 +0200

    fix: add missing French button labels and fix a config typo

--- I18n/backOffice/default/en_US.php
+++ I18n/backOffice/default/en_US.php
@@ -1,5 +1,8 @@
 <?php
 return array(
+    'Cancel' => 'Cancel',
     'Configuration correctly saved' => 'Configuration successfully saved !',
     'Configure mailjet' => 'Configure Mailjet',
+    'Save' => 'Save',
+    'Save and close' => 'Save and close',
 );
--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -1,5 +1,8 @@
 <?php
 return array(
+    'Cancel' => 'Annuler',
     'Configuration correctly saved' => 'La configuration a bien été enregistrée !',
     'Configure mailjet' => 'Configuration Mailjet',
+    'Save' => 'Enregistrer',
+    'Save and close' => 'Enregistrer et fermer',
 );
